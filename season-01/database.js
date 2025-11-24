a42f4b642d8b4a3f35774805b4ca7a84e9d3b714
Kavita Mahato <kavitamahato0835@gmail.com>
2025-11-24 09:08:02 +0530

    CRUD Operations

--- season-01/database.js
+++ season-01/database.js
@@ -12,7 +12,33 @@ async function main() {
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
-    const collection = db.collection("user");
+    const collection = db.collection("User");
+
+    // Insert data
+    // const data = {
+    //     firstname: "Antara",
+    //     lastname: "Mahanty",
+    //     city: "Kolkata",
+    //     phoneNumber: "456789"
+    // }
+    // const insertResult = await collection.insertMany([data]);
+    // console.log('Inserted documents =>', insertResult);
+
+    // Read 
+    const findResult = await collection.find({}).toArray();
+    console.log("Found documents =>", findResult);
+
+    // Update
+    // const updateResult = await collection.updateOne({ firstname: "Antara" }, { $set: { phoneNumber: "9800980098" } });
+    // console.log('Updated documents =>', updateResult);
+
+    // Delete
+    // const deleteResult = await collection.deleteMany({ phoneNumber: "1023456789" });
+    // console.log('Deleted documents =>', deleteResult);
+
+    // Count
+    const countResult = await collection.countDocuments({});
+    console.log("Total documents in User collection =>", countResult);
 
     return "done.";
 }
