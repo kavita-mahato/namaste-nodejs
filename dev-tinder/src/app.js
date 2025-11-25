f0b9e69f0f3ab7d179291859390dd272789a3fb2
Kavita Mahato <kavitamahato0835@gmail.com>
2025-11-25 20:01:00 +0530

    Create Express Server

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -1,1 +1,19 @@
-console.log("Starting a new Project!");
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+    res.send("Hello from the server");
+});
+
+app.use("/hello", (req, res) => {
+    res.send("Hello what's up!");
+});
+
+app.use("/", (req, res) => {
+    res.send("Hello from the dashboard!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000...");
+});
