85c70fb04f04fb73fc43b9c36756f90784e623b4
Kavita Mahato <kavitamahato0835@gmail.com>
2025-11-29 20:10:01 +0530

    Established database connection

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -1,31 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    // Log your error
-    res.status(500).send("something went wrong");
-  }
-});
-
-app.get("/getUserData", (req, res) => {
-  //try {
-  // Logic of DB call and get user data
-  throw new Error("dvbzhjf");
-  res.send("User Data Sent");
-  //   } catch (err) {
-  //     res.status(500).send("Some Error contact support team");
-  //   }
-});
-
-// Error handling middleware should be defined at the end of all routes and middlewares
-// because it will catch errors from them
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("something went wrong");
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000...");
-});
+// Connect to database first, then start server
+connectDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err.message);
+    process.exit(1);
+  });
---
+++ dev-tinder/src/config/database.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../../.env") });
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in .env file");
+    }
+    
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connection established...");
+    return true;
+  } 
+  catch (error) {
+    console.error("Database cannot be connected!!");
+    console.error("Error details:", error.message);
+    throw error;
+  }
+};
+
+module.exports = connectDB;
