980fc297b8e551efc596870211da1cd33085622f
Kavita Mahato <kavitamahato0835@gmail.com>
2025-12-04 07:04:34 +0530

    Logical DB Query

--- dev-tinder/src/routes/requests.js
+++ dev-tinder/src/routes/requests.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async(req, res) => {
-  try{
-    const user = req.user;
-    console.log("Sending connection request");
-    res.send(user.firstName +" " + user.lastName + " sent connection request!");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req, res) => {
+     try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      // Validate status
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      // Check if the toUserId exists
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+
+      // Check if a connection request already exists between the two users
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists!!" });
+      }
+
+      // Create a new connection request
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      // Save the connection request to the database
+      const data = await connectionRequest.save();
+
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
   }
   catch(err){
     res.status(400).send("ERROR : " + err.message);
